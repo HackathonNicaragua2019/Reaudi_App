7d6d692a36553ae14c2e314cfa1c92d1de2b5d70
Edycar Reyes <edycarreyes@gmail.com>
2019-11-10 01:33:56 -0600

    Se termina de disenar la parte del AppBar del componente del HomePage

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:reaudi/customcolors/custom-colors.dart';
 import 'package:reaudi/services/auth.dart';
+import 'package:bubble_tab_indicator/bubble_tab_indicator.dart';
 
 class HomePage extends StatefulWidget {
   HomePage({Key key, this.titulo, this.auth, this.userId, this.onSignedOut})
@@ -17,6 +18,8 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  CustomColors customColors = new CustomColors();
+
   @override
   void initState() {
     // TODO: implement initState
@@ -25,14 +28,105 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.titulo),
-      ),
-      body: Center(
-        child: Text('Hola mundo'),
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        drawer: Drawer(),
+        appBar: AppBar(
+          // Here we take the value from the MyHomePage object that was created by
+          // the App.build method, and use it to set our appbar title.
+          title: Text(widget.titulo),
+
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.search),
+              onPressed: () => print('Busqueda presionado'),
+              color: Colors.white,
+            ),
+            IconButton(
+              icon: Icon(Icons.sort),
+              onPressed: () => print('Filtro presionado'),
+            )
+          ],
+
+          bottom: TabBar(
+            indicatorSize: TabBarIndicatorSize.label,
+            isScrollable: true,
+            indicator: BubbleTabIndicator(
+              indicatorHeight: 33.0,
+              indicatorColor: customColors.primarySwatch,
+              tabBarIndicatorSize: TabBarIndicatorSize.label,
+            ),
+            tabs: <Widget>[
+              Tab(
+                child: Padding(
+                  padding: const EdgeInsets.only(
+                    left: 15,
+                    right: 15,
+                  ),
+                  child: Text('Frecuentes'),
+                ),
+              ),
+              Tab(
+                child: Padding(
+                  padding: const EdgeInsets.only(
+                    left: 15,
+                    right: 15,
+                  ),
+                  child: Text('Más Populares'),
+                ),
+              ),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: <Widget>[
+            ListView(
+              children: <Widget>[
+                Card(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      const ListTile(
+                        leading: Icon(Icons.album),
+                        title: Text('The Enchanted Nightingale'),
+                        subtitle: Text(
+                            'Music by Julie Gable. Lyrics by Sidney Stein.'),
+                      ),
+                      ButtonTheme.bar(
+                        // make buttons use the appropriate styles for cards
+                        child: ButtonBar(
+                          children: <Widget>[
+                            FlatButton(
+                              child: const Text('BUY TICKETS'),
+                              onPressed: () {
+                                /* ... */
+                              },
+                            ),
+                            FlatButton(
+                              child: const Text('LISTEN'),
+                              onPressed: () {
+                                /* ... */
+                              },
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            Center(
+              child: Text('Hola Mundo'),
+            )
+            /*AccountPage(
+              auth: widget.auth,
+              userId: widget.userId,
+              onSignedOut: widget.onSignedOut,
+            )*/
+          ],
+        ),
       ),
     );
   }
